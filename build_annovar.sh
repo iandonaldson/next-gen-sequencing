e8f647289e32ed66ab7c7d05476ead8fc4bc0e66
Ian Donaldson <ian@donaldsonresearch.com>
2016-05-23 13:38:18 +0100

    Create build_annovar.sh

---
+++ build_annovar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#build_annovar.sh
+
+HUMANDB="/data/home/wgw057/scratch/annovardb/hg19"
+
+mkdir -p ${HUMANDB}
+
+module load annovar
+
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar refGene ${HUMANDB}/
+
+annotate_variation.pl -buildver hg19 -downdb cytoBand ${HUMANDB}/
+
+annotate_variation.pl -buildver hg19 -downdb genomicSuperDups ${HUMANDB}/ 
+
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar esp6500siv2_all ${HUMANDB}/
+
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar 1000g2014oct ${HUMANDB}/
+
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar snp138 ${HUMANDB}/ 
+
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar ljb26_all ${HUMANDB}/
